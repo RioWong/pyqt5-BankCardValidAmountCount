a66c345146f8cfbb69f96f4992410b39ecdade32
unknown <951683309@qq.com>
2018-10-12 16:31:25 +0800

    v7.7

--- banktotal/exclude_relatives.py
+++ banktotal/exclude_relatives.py
@@ -1,6 +1,12 @@
-"""remark、nameOnOppositeCard以及transAddr 中，
-将含关键字客户名、配偶名、客户公司名、客户公司股东姓名、或指定的客户亲属姓名 的记录标记为无效流水"""
+"""
 
+1 remark、nameOnOppositeCard以及transAddr 中，
+将含关键字“客户名/配偶名/客户公司名/客户公司股东姓名 |指定客户亲属姓名”字样标记为无效流水
+
+2 description列中若含有"客户名+支付信转账"，则判定为无效流水
+
+"""
+import pandas as pd
 
 def exclude_relatives(df, relatives_lst):
     # 如果打算对 "description"列也进行筛选，将use_descripton的值改为"是"
@@ -18,10 +24,18 @@ def exclude_relatives(df, relatives_lst):
         df['tmp_col'] = df['remark'] + "|||" + df['nameOnOppositeCard'] + "|||" + df['transAddr']
     else:
         df['tmp_col'] = df['remark'] + "|||" + df['nameOnOppositeCard'] + "|||" + \
-                    df['transAddr'] + "|||" + df['description']
+                        df['transAddr'] + "|||" + df['description']
     exclude_words = "|".join(relatives_lst)
-    drop_df = df[df['tmp_col'].str.contains(exclude_words)]
-    keep_df = df[~df['tmp_col'].str.contains(exclude_words)]
+    drop_df1 = df[df['tmp_col'].str.contains(exclude_words)]
+    keep_df1 = df[~df['tmp_col'].str.contains(exclude_words)]
+
+    # 新增规则：description列中若含有"客户名+支付宝转账"，则判定为无效流水
+
+    pat2 = r'{name}支付宝转账'.format(name=relatives_lst[0])
+    keep_df = keep_df1[~keep_df1['description'].str.contains(pat2)]
+    drop_df2 = keep_df1[keep_df1['description'].str.contains(pat2)]
+
+    drop_df = pd.concat([drop_df1, drop_df2])
     del keep_df['tmp_col']
     del drop_df['tmp_col']
     return keep_df, drop_df
